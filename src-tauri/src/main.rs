c8cef01d272fdcbe94b3a8f502085d017520b7a8
Tiggyin <160556904+Tiggyin@users.noreply.github.com>
2026-05-27 00:02:37 +0000

    Fix formatting for CI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,4 @@
+cat > src-tauri/src/main.rs << 'EOF'
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -858,3 +859,4 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+EOF
